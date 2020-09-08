b2500d31ddf7e09e0a731e9e09902a79bb4f7332
David Shorthouse <davidpshorthouse@gmail.com>
2020-09-08 16:42:15 -0400

    Get latest elasticsearch for travis

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,2 +1,16 @@
 describe "Bionomia Administration Controller" do
+  before(:each) do
+    @user = User.create!({ given: "John", family: "Smith", other_names: "Jack", is_admin: true })
+    env 'rack.session', csrf: 'token', omniauth: OpenStruct.new({ id: @user.id })
+  end
+
+  after(:each) do
+    @user.destroy
+  end
+
+  it "should allow access to the admin welcome page" do
+    get '/admin'
+    expect(last_response).to be_ok
+  end
+
 end
