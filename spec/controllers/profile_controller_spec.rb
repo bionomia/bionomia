eb19a7a5c60c5c06d2ae4627e01f0c2029c26484
David Shorthouse <davidpshorthouse@gmail.com>
2020-09-08 19:04:29 -0400

    Add profile routes to spec tests

---
+++ spec/controllers/profile_controller_spec.rb
@@ -0,0 +1,56 @@
+describe "Bionomia Profile Controller" do
+  before(:each) do
+    @user = User.create!({ given: "John", family: "Smith", other_names: "Jack" })
+    env 'rack.session', csrf: 'token', omniauth: OpenStruct.new({ id: @user.id })
+  end
+
+  after(:each) do
+    @user.destroy
+  end
+
+  it "should allow access to the profile page" do
+    get '/profile'
+    expect(last_response).to be_ok
+  end
+
+  it "should allow access to the profile settings page" do
+    get '/profile/settings'
+    expect(last_response).to be_ok
+  end
+
+  it "should allow access to the profile specimens" do
+    get '/profile/specimens'
+    expect(last_response).to be_ok
+  end
+
+  it "should allow access to the profile support" do
+    get '/profile/support'
+    expect(last_response).to be_ok
+  end
+
+  it "should allow access to the profile helped" do
+    get '/profile/helped'
+    expect(last_response).to be_ok
+  end
+
+  it "should allow access to the profile messages" do
+    get '/profile/messages'
+    expect(last_response).to be_ok
+  end
+
+  it "should allow access to the profile upload" do
+    get '/profile/upload'
+    expect(last_response).to be_ok
+  end
+
+  it "should allow access to the profile ignored" do
+    get '/profile/ignored'
+    expect(last_response).to be_ok
+  end
+
+  it "should allow access to the profile citations" do
+    get '/profile/citations'
+    expect(last_response).to be_ok
+  end
+
+end
