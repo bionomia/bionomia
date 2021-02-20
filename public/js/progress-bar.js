0e57457f72addf8bd23fc213a70ab9d1d7c1ee58
David Shorthouse <davidpshorthouse@gmail.com>
2021-02-20 12:53:02 -0500

    Change progress bar execution

--- public/js/progress-bar.js
+++ public/js/progress-bar.js
@@ -10,38 +10,38 @@ var ProgressBar = (function($, window) {
     init: function(identifier, path = "user") {
       this.identifier = typeof identifier !== 'undefined' ? identifier : "";
       this.path = path;
-      this.candidate_counter();
+      return this.candidate_counter();
     },
 
     candidate_counter: function() {
       var self = this, denominator, percent, message, progress_bar = $('#progress-bar_' + this.identifier),
           path = (this.path == "user") ? "" : this.path + "/";
-      $.ajax({
-        method: "GET",
-        url: "/" + path + self.identifier + "/progress.json"
-      }).done(function(data) {
-        denominator = data.claimed + data.unclaimed;
-        if (denominator === 0) {
-          percent = 100;
-          message = "None";
-        } else {
-          percent = parseInt(100 * data.claimed / denominator, 10);
-          message = percent + "%";
-        }
-        progress_bar.width(percent + '%').text(message);
-        if (message === "None") {
-          progress_bar.removeClass("bg-info").addClass("bg-secondary");
-        }
-        if (percent === 100 && denominator > 0) {
-          progress_bar.removeClass("bg-info").addClass("bg-success");
-        }
-      });
+      return $.ajax({
+          method: "GET",
+          url: "/" + path + self.identifier + "/progress.json"
+        }).done(function(data) {
+          denominator = data.claimed + data.unclaimed;
+          if (denominator === 0) {
+            percent = 100;
+            message = "None";
+          } else {
+            percent = parseInt(100 * data.claimed / denominator, 10);
+            message = percent + "%";
+          }
+          progress_bar.width(percent + '%').text(message);
+          if (message === "None") {
+            progress_bar.removeClass("bg-info").addClass("bg-secondary");
+          }
+          if (percent === 100 && denominator > 0) {
+            progress_bar.removeClass("bg-info").addClass("bg-success");
+          }
+        });
     }
   };
 
   return {
     init: function(identifier, path) {
-      _private.init(identifier, path);
+      return _private.init(identifier, path);
     }
   };
 
