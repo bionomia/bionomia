7921029b5b8c7c8528a427d531638e968e9cbaee
David Shorthouse <davidpshorthouse@gmail.com>
2021-03-21 09:31:49 -0400

    Set strict paths to false

--- application.rb
+++ application.rb
@@ -9,6 +9,7 @@ class BIONOMIA < Sinatra::Base
   set :public_folder, 'public'
   set :cache_enabled_in, [:development, :production]
   set :protection, :except => [:json_csrf, :remote]
+  set :strict_paths, false
 
   register Config
   register Sinatra::I18nSupport
