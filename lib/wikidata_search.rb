d23753ba99ed163f6f6d04e6fe7bfc2f195c06e3
David Shorthouse <davidpshorthouse@gmail.com>
2021-04-19 20:21:07 -0400

    Add timeouts for wikidata lib, hoping to fix error

--- lib/wikidata_search.rb
+++ lib/wikidata_search.rb
@@ -12,6 +12,15 @@ module Bionomia
       "Stuttgart Database of Scientific Illustrators ID": "P2349"
     }
 
+    Wikidata.configure do |config|
+      config.options = {
+        request: {
+          timeout: 20,
+          open_timeout: 20
+        }
+      }
+    end
+
     def initialize
       headers = { 'User-Agent' => 'Bionomia/1.0' }
       @sparql = SPARQL::Client.new("https://query.wikidata.org/sparql", headers: headers, read_timeout: 120)
