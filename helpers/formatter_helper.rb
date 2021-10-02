b54ec2897e470824c9a68318010bebe269c2986e
David Shorthouse <davidpshorthouse@gmail.com>
2021-10-02 17:36:58 -0400

    Fix a href link helper

--- helpers/formatter_helper.rb
+++ helpers/formatter_helper.rb
@@ -35,9 +35,9 @@ module Sinatra
         def link_to link_text, url, mode=:path_only
           if(url_for(url,mode)[0,2] == "!!")
             trimmed_url = url_for(url,mode)[2..-1]
-            "<a href=#{trimmed_url}> #{link_text}</a>"
+            "<a href=\"#{trimmed_url}\">#{link_text}</a>"
           else
-            "<a href=#{url_for(url,mode)}> #{link_text}</a>"
+            "<a href=\"#{url_for(url,mode)}\">#{link_text}</a>"
           end
         end
 
