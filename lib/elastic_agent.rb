d97e4e89690e09aaefdfc2241667c1b22272e054
David Shorthouse <davidpshorthouse@gmail.com>
2021-10-02 07:09:05 -0400

    Adjust search analyzer to split on commas as well

--- lib/elastic_agent.rb
+++ lib/elastic_agent.rb
@@ -25,7 +25,7 @@ module Bionomia
             tokenizer: {
               simple_split: {
                 type: "simple_pattern_split",
-                pattern: "\\.| "
+                pattern: "\\.|\\,| "
               }
             },
             analyzer: {
