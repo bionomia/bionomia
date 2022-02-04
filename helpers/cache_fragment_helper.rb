5c8c65de8eceebe64ac8bde0cdfca27170b1eb92
David Shorthouse <davidpshorthouse@gmail.com>
2022-02-04 01:27:09 -0500

    Fix tests

--- helpers/cache_fragment_helper.rb
+++ helpers/cache_fragment_helper.rb
@@ -11,11 +11,17 @@ module Sinatra
         tag = "fragments/#{tag}"
         unless opts[:overwrite]
           content, = settings.cache_get_tag(tag)
-          return block_is_template?(blk) ? concat_content(content.force_encoding(Encoding::UTF_8)) : content.force_encoding(Encoding::UTF_8) if content
+          if !content.nil?
+            content = content.force_encoding(Encoding::UTF_8)
+          end
+          return block_is_template?(blk) ? concat_content(content) : content if content
         end
         content = capture_html(&blk)
         settings.cache_put_tag(tag, content)
-        block_is_template?(blk) ? concat_content(content.force_encoding(Encoding::UTF_8)) : content.force_encoding(Encoding::UTF_8)
+        if !content.nil?
+          content = content.force_encoding(Encoding::UTF_8)
+        end
+        block_is_template?(blk) ? concat_content(content) : content
       end
 
     end
