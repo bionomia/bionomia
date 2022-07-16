62fa44abcb692e0796e5933a9892c174d8ceea80
David Shorthouse <davidpshorthouse@gmail.com>
2022-07-16 12:33:13 -0400

    Temporary workaround for Symbol as keys in ActiveRecord serialize

--- models/initialize.rb
+++ models/initialize.rb
@@ -22,6 +22,8 @@ module Sinatra
           hash[:default_env] = config
           db_configs = ActiveRecord::DatabaseConfigurations.new hash
 
+          ActiveRecord.use_yaml_unsafe_load = true
+
           ActiveRecord::Base.configurations = db_configs
           ActiveRecord::Base.establish_connection
 
