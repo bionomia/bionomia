ebef313996e1a15ba717e808252e2a4efb64a6ee
David Shorthouse <davidpshorthouse@gmail.com>
2023-01-27 01:21:06 -0500

    Adjust claim worker to use method in DestroyedUser

--- lib/existing_claims_worker.rb
+++ lib/existing_claims_worker.rb
@@ -20,11 +20,9 @@ module Bionomia
       row["agentIDs"].split("|").map(&:strip).uniq.each do |id|
         next if id.empty?
         u = get_user(id)
+        next if u.nil?
         next if !u.nil? && User::BOT_IDS.include?(u.id)
-        if u.nil?
-          #TODO: write to a log file for id that could not be found
-          next
-        end
+
         if !uniq_recs.empty?
           uo = uniq_recs.map{|r| [u.id, r.to_i, "recorded", User::GBIF_AGENT_ID]}
           import_user_occurrences(uo)
@@ -42,61 +40,38 @@ module Bionomia
 
     def get_user(id)
       user = nil
+      wiki = ::Bionomia::WikidataSearch.new
+
       if id.wiki_from_url
         user = get_wiki_user(id.wiki_from_url) rescue nil
-      end
-      if id.orcid_from_url && id.orcid_from_url.is_orcid?
+      elsif id.orcid_from_url && id.orcid_from_url.is_orcid?
         user = get_orcid_user(id.orcid_from_url) rescue nil
-      end
-      if id.viaf_from_url
-        w = ::Bionomia::WikidataSearch.new
-        wikidata = w.wiki_by_property('viaf', id.viaf_from_url)[:wikidata] rescue nil
-        if wikidata
-          user = get_wiki_user(wikidata)
-        end
-      end
-      if id.isni_from_url
-        w = ::Bionomia::WikidataSearch.new
-        wikidata = w.wiki_by_property('isni', id.isni_from_url)[:wikidata] rescue nil
-        if wikidata
-          user = get_wiki_user(wikidata)
-        end
-      end
-      if id.ipni_from_url
-        w = ::Bionomia::WikidataSearch.new
-        wikidata = w.wiki_by_property('ipni', id.ipni_from_url)[:wikidata] rescue nil
-        if wikidata
-          user = get_wiki_user(wikidata)
-        end
-      end
-      if id.bhl_from_url
-        w = ::Bionomia::WikidataSearch.new
-        wikidata = w.wiki_by_property('bhl', id.bhl_from_url)[:wikidata] rescue nil
-        if wikidata
-          user = get_wiki_user(wikidata)
-        end
-      end
-      if id.zoobank_from_url
-        w = ::Bionomia::WikidataSearch.new
-        wikidata = w.wiki_by_property('zoobank', id.zoobank_from_url)[:wikidata] rescue nil
-        if wikidata
-          user = get_wiki_user(wikidata)
-        end
-      end
-      if id.library_congress_from_url
-        w = ::Bionomia::WikidataSearch.new
-        wikidata = w.wiki_by_property('congress', id.library_congress_from_url)[:wikidata] rescue nil
-        if wikidata
-          user = get_wiki_user(wikidata)
-        end
+      elsif id.viaf_from_url
+        wikidata = wiki.wiki_by_property('viaf', id.viaf_from_url)[:wikidata] rescue nil
+        user = get_wiki_user(wikidata) if wikidata
+      elsif id.isni_from_url
+        wikidata = wiki.wiki_by_property('isni', id.isni_from_url)[:wikidata] rescue nil
+        user = get_wiki_user(wikidata) if wikidata
+      elsif id.ipni_from_url
+        wikidata = wiki.wiki_by_property('ipni', id.ipni_from_url)[:wikidata] rescue nil
+        user = get_wiki_user(wikidata) if wikidata
+      elsif id.bhl_from_url
+        wikidata = wiki.wiki_by_property('bhl', id.bhl_from_url)[:wikidata] rescue nil
+        user = get_wiki_user(wikidata) if wikidata
+      elsif id.zoobank_from_url
+        wikidata = wiki.wiki_by_property('zoobank', id.zoobank_from_url)[:wikidata] rescue nil
+        user = get_wiki_user(wikidata) if wikidata
+      elsif id.library_congress_from_url
+        wikidata = wiki.wiki_by_property('congress', id.library_congress_from_url)[:wikidata] rescue nil
+        user = get_wiki_user(wikidata) if wikidata
       end
       user
     end
 
     def get_orcid_user(id)
-      destroyed = DestroyedUser.find_by_identifier(id)
-      if !destroyed.nil? && !destroyed.redirect_to.nil?
-        user = User.find_by_identifier(destroyed.redirect_to)
+      d = DestroyedUser.active_user_identifier(id)
+      if !d.nil?
+        user = User.find_by_identifier(d)
       else
         user = User.create_with({ orcid: id })
                    .find_or_create_by({ orcid: id })
@@ -105,25 +80,16 @@ module Bionomia
     end
 
     def get_wiki_user(id)
-      destroyed = DestroyedUser.find_by_identifier(id)
-      if !destroyed.nil? && !destroyed.redirect_to.nil?
-        user = User.find_by_identifier(destroyed.redirect_to)
+      d = DestroyedUser.active_user_identifier(id)
+      if !d.nil?
+        user = User.find_by_identifier(d)
       else
-        w = ::Bionomia::WikidataSearch.new
-        if w.wiki_user_data(id)[:date_died].nil?
+        user = User.create_with({ wikidata: id, is_public: true, made_public: Time.now })
+                   .find_or_create_by({ wikidata: id })
+        if !user.valid_wikicontent?
+          user.delete_search
+          user.delete
           user = nil
-        else
-          user = User.create_with({ wikidata: id })
-                     .find_or_create_by({ wikidata: id })
-          if !user.valid_wikicontent?
-            user.delete_search
-            user.delete
-            user = nil
-          elsif user.valid_wikicontent? && !user.is_public?
-            user.is_public = true
-            user.made_public = Time.now
-            user.save
-          end
         end
       end
       user
