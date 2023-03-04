698c3dc4adaee773c1d90b478ceea61b901bfc7c
David Shorthouse <davidpshorthouse@gmail.com>
2023-03-04 14:04:35 -0500

    Use parallel to generate orphaned occurrences; prevent click on countries with no records

--- models/orphaned_user_occurrence.rb
+++ models/orphaned_user_occurrence.rb
@@ -3,18 +3,16 @@ class OrphanedUserOccurrence < ActiveRecord::Base
 
   def self.rebuild
     delete_all
-    pbar = ProgressBar.create(title: "Orphaned", total: (UserOccurrence.count/10_000).to_i, autofinish: false, format: '%t %b>> %i| %e')
-    UserOccurrence.in_batches(of: 10_000) do |relation|
-      pbar.increment
+
+    Parallel.each(UserOccurrence.in_batches(of: 10_000), progress: "Building orphaned", in_threads: 4) do |relation|
       relation.where.missing(:occurrence).each do |uo|
         self.create({
           occurrence_id: uo.occurrence_id,
           user_id: uo.user_id,
           created_by: uo.created_by
-      })
+        })
       end
     end
-    pbar.finish
   end
  
  end
