32053f109b34168ac904e89bf1bfa931c3201bdd
David Shorthouse <davidpshorthouse@gmail.com>
2023-06-10 12:24:50 -0400

    More enhancements to scheduled submit to Zenodo

--- bin/zenodo.rb
+++ bin/zenodo.rb
@@ -8,11 +8,11 @@ options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: zenodo.rb [options]"
 
-  opts.on("-n", "--new", "Push brand new claims data to Zenodo") do
+  opts.on("-n", "--new", "Push brand new claims data to Zenodo for ORCID-based profiles") do
     options[:new] = true
   end
 
-  opts.on("-w", "--within-week", "Push new versions to Zenodo but only accounts updated within last week.") do
+  opts.on("-w", "--within-week", "Push to Zenodo for accounts that have a recent claim/attribution.") do
     options[:within_week] = true
   end
 
@@ -20,7 +20,7 @@ OptionParser.new do |opts|
     options[:identifier] = identifier
   end
 
-  opts.on("-r", "--refresh", "Refresh all Zenodo tokens") do
+  opts.on("-r", "--refresh", "Refresh all Zenodo tokens for ORCID-based profiles") do
     options[:refresh] = true
   end
 
@@ -106,31 +106,39 @@ def submit_update(u)
   end
 end
 
-if options[:new] && options[:identifier]
-  u = User.find_by_identifier(options[:identifier])
-  return if u.nil? || !u.zenodo_doi.nil?
-  submit_new(u)
+if options[:new]
+  if options[:identifier]
+    u = User.find_by_identifier(options[:identifier])
+    return if u.nil? || !u.zenodo_doi.nil?
+    submit_new(u)
+  elsif !options[:identifier]
+    User.where.not(zenodo_access_token: nil)
+        .where(zenodo_doi: nil).find_each do |u|
+          submit_new(u)
+    end
 
-elsif options[:new] && !options[:identifier]
-  User.where.not(zenodo_access_token: nil)
-      .where(zenodo_doi: nil).find_each do |u|
-        submit_new(u)
-  end
-
-elsif options[:identifier] && !options[:new]
+elsif !options[:new] && options[:identifier]
   u = User.find_by_identifier(options[:identifier])
   return if u.nil? || u.zenodo_doi.nil? || (u.orcid && u.zenodo_access_token.nil?)
   submit_update(u)
 
 elsif options[:within_week]
   week_ago = DateTime.now - 7.days
-  user_ids = UserOccurrence.select("user_occurrences.user_id AS user_id, MAX(user_occurrences.created) AS created")
-                           .where("user_occurrences.created >= '#{week_ago}'")
-                           .group("user_occurrences.user_id")
+  user_ids = UserOccurrence.select(:user_id, "MAX(created) AS created")
+                           .where("created >= '#{week_ago}'")
+                           .group(:user_id)
   User.where(id: user_ids.map(&:user_id))
-      .where.not(zenodo_doi: nil)
       .find_each do |u|
-        submit_update(u)
+        if u.zenodo_doi && u.orcid
+          submit_update(u)
+        elsif u.zenodo_doi && u.wikidata
+          latest = u.visible_user_occurrences.order(created: :desc).limit(1).first rescue nil
+          next if latest.nil? || User::BOT_IDS.include?(latest.created_by)
+          submit_update(u)  
+        elsif u.zenodo_doi.nil? && u.wikidata
+          latest = u.visible_user_occurrences.order(created: :desc).limit(1).first rescue nil
+          next if latest.nil? || User::BOT_IDS.include?(latest.created_by)
+          submit_new(u)
       end
 end
 
