aa272b3df296e08f685aafc87bffdf3918cbe6cf
David Shorthouse <davidpshorthouse@gmail.com>
2023-06-09 20:30:51 -0400

    Adjust bin to submit new DOI to Zenodo

--- bin/zenodo.rb
+++ bin/zenodo.rb
@@ -34,38 +34,46 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-# TODO: incorporate a check here for wikidata-based accounts too
-if options[:new]
+def submit_new(u)
+  z = Bionomia::Zenodo.new(user: u)
+  begin
+    u.skip_callbacks = true
+
+    if u.orcid
+      u.zenodo_access_token = z.refresh_token
+      u.save
+    end
+
+    doi_id = z.new_deposit
+    id = doi_id[:recid]
+    io = Bionomia::IO.new({ user: u })
+    csv = io.csv_stream_occurrences(u.visible_occurrences)
+    z.add_file_enum(id: id, enum: csv, file_name: u.identifier + ".csv")
+    temp = Tempfile.new
+    temp.binmode
+    io.jsonld_stream("all", temp)
+    z.add_file(id: id, file_path: temp.path, file_name: u.identifier + ".json")
+    temp.close
+    temp.unlink
+    pub = z.publish(id: id)
+    u.zenodo_doi = pub[:doi]
+    u.zenodo_concept_doi = pub[:conceptdoi]
+    u.save
+    puts "#{u.viewname}".green
+  rescue
+    puts "#{u.viewname} (id=#{u.id}) token failed".red
+  end
+end
+
+if options[:new] && options[:identifier]
+  u = User.find_by_identifier(options[:identifier])
+  return if u.nil? || !u.zenodo_doi.nil?
+  submit_new(u)
+
+elsif options[:new]
   User.where.not(zenodo_access_token: nil)
       .where(zenodo_doi: nil).find_each do |u|
-    z = Bionomia::Zenodo.new(user: u)
-    begin
-      u.skip_callbacks = true
-
-      if u.orcid
-        u.zenodo_access_token = z.refresh_token
-        u.save
-      end
-
-      doi_id = z.new_deposit
-      id = doi_id[:recid]
-      io = Bionomia::IO.new({ user: u })
-      csv = io.csv_stream_occurrences(u.visible_occurrences)
-      z.add_file_enum(id: id, enum: csv, file_name: u.identifier + ".csv")
-      temp = Tempfile.new
-      temp.binmode
-      io.jsonld_stream("all", temp)
-      z.add_file(id: id, file_path: temp.path, file_name: u.identifier + ".json")
-      temp.close
-      temp.unlink
-      pub = z.publish(id: id)
-      u.zenodo_doi = pub[:doi]
-      u.zenodo_concept_doi = pub[:conceptdoi]
-      u.save
-      puts "#{u.viewname}".green
-    rescue
-      puts "#{u.viewname} (id=#{u.id}) token failed".red
-    end
+        submit_new(u)
   end
 
 elsif options[:identifier]
