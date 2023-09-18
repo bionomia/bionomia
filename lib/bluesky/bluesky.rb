41fed9b82c63279e5f7d7868de7114c483791075
David Shorthouse <davidpshorthouse@gmail.com>
2023-09-18 19:58:17 -0400

    WIP bluesky library

--- lib/bluesky/bluesky.rb
+++ lib/bluesky/bluesky.rb
@@ -5,23 +5,32 @@ module Bionomia
 
     def initialize
       @session = get_session
+      @post_item = {}
+      @image_items = []
     end
 
-    def add_file(file_path:, file_name: nil)
-    end
-
-    def post(text:)
-      url = Settings.bluesky.endpoint + "com.atproto.repo.createRecord"
-      item = {
+    def add_text(text:)
+      @post_item = {
          "$type": "app.bsky.feed.post",
          text: text,
          createdAt: Time.now.iso8601,
-         langs: ["en-US"],
+         langs: ["en-US"]
       }
+    end
+
+    def add_image(image:) 
+      #TODO: get image, make tmp object, resize it, upload it, flush it and add to @image_items array
+      #TODO: add an embed object to @post_item if not present, otherwise add to images array there.
+      # See https://atproto.com/blog/create-post
+    end
+
+    def post
+      #TODO: throw exception of @post_item is an empty object
+      url = Settings.bluesky.endpoint + "com.atproto.repo.createRecord"
       payload = {
          repo: @session[:did],
          collection: "app.bsky.feed.post",
-         record: item
+         record: @post_item
       }
       response = RestClient::Request.execute(
          method: :post,
