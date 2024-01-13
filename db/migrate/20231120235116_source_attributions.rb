0d30ba624da4bb92e3841c4ac029151660a063a4
David Shorthouse <davidpshorthouse@gmail.com>
2024-01-13 14:03:52 -0500

    Fix migration

--- db/migrate/20231120235116_source_attributions.rb
+++ db/migrate/20231120235116_source_attributions.rb
@@ -3,7 +3,7 @@ class SourceAttributions < ActiveRecord::Migration[7.0]
     create_table :source_users, if_not_exists: true do |t|
       t.string :identifier, null: false
     end
-    if column_exists?(:source_users, :identifier)
+    if column_exists?(:source_users, :identifier) && !index_exists?(:source_users, :identifier)
       add_index :source_users, :identifier, unique: true
     end
     create_table :source_attributions, if_not_exists: true do |t|
@@ -11,7 +11,7 @@ class SourceAttributions < ActiveRecord::Migration[7.0]
       t.bigint :occurrence_id, null: false
       t.string :action, null: false
     end
-    if column_exists?(:source_attributions, :user_id)
+    if column_exists?(:source_attributions, :user_id) && !index_exists?(:source_attributions, [:user_id, :occurrence_id, :action], name: 'source_attributions_composite')
       add_index :source_attributions, [:user_id, :occurrence_id, :action], name: 'source_attributions_composite', unique: true
     end
   end
