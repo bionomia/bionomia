290a0e9d7d5bda786cc40c7d45b022a865c16367
David Shorthouse <davidpshorthouse@gmail.com>
2024-02-26 00:18:25 -0500

    Adjust how datasets get pushed to Zenodo

--- bin/zenodo_dataset.rb
+++ bin/zenodo_dataset.rb
@@ -41,8 +41,7 @@ if options[:uuid]
       submit_update(d)
    end
 elsif options[:refresh]
-   week_ago = DateTime.now - 7.days
-   Dataset.where("frictionless_created_at <= '#{week_ago}'").find_each do |d|
+   Dataset.not(frictionless_created_at: nil).find_each do |d|
       next if d.nil? || !d.has_claim?
       if d.zenodo_doi.nil?
          submit_new(d)
