38cd5480cb95ca7986fbfdef9764e36f69dd53da
David Shorthouse <davidpshorthouse@gmail.com>
2024-06-23 13:22:36 -0400

    Remove spec tests for agent model for now

--- spec/models/agent_spec.rb
+++ spec/models/agent_spec.rb
@@ -1,18 +1,3 @@
 describe "Agent model" do
 
-  it "is not valid without given content" do
-    agent = Agent.new(given: nil, family: "", unparsed: "")
-    expect(agent).to_not be_valid
-  end
-
-  it "is not valid without family content" do
-    agent = Agent.new(given: "", family: nil, unparsed: "")
-    expect(agent).to_not be_valid
-  end
-
-  it "is not valid without unparsed content" do
-    agent = Agent.new(given: "", family: "", unparsed: nil)
-    expect(agent).to_not be_valid
-  end
-
 end
