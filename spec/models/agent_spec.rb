3bcbca23c22b2747b66744d415e82e75a322b91f
David Shorthouse <davidpshorthouse@gmail.com>
2024-06-23 13:19:00 -0400

    Same as before

--- spec/models/agent_spec.rb
+++ spec/models/agent_spec.rb
@@ -1,17 +1,17 @@
 describe "Agent model" do
 
   it "is not valid without given content" do
-    agent = Agent.new(given: nil, family: "", parsed: "")
+    agent = Agent.new(given: nil, family: "", unparsed: "")
     expect(agent).to_not be_valid
   end
 
   it "is not valid without family content" do
-    agent = Agent.new(given: "", family: nil, parsed: "")
+    agent = Agent.new(given: "", family: nil, unparsed: "")
     expect(agent).to_not be_valid
   end
 
-  it "is not valid without parsed content" do
-    agent = Agent.new(given: "", family: "", parsed: nil)
+  it "is not valid without unparsed content" do
+    agent = Agent.new(given: "", family: "", unparsed: nil)
     expect(agent).to_not be_valid
   end
 
