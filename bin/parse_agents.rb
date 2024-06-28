94d5d217835c87147ff83c5a413eba00cafcf32c
David Shorthouse <davidpshorthouse@gmail.com>
2024-06-28 18:02:58 -0400

    Add queue param to agent scripts

--- bin/parse_agents.rb
+++ bin/parse_agents.rb
@@ -4,8 +4,13 @@ require File.dirname(File.dirname(__FILE__)) + '/application.rb'
 
 ARGV << '-h' if ARGV.empty?
 
+options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: parse_agents.rb"
+
+  opts.on("-q", "--queue", "Queue the jobs to parse agent strings.") do
+    options[:queue] = true
+  end
 
   opts.on("-h", "--help", "Prints this help") do
     puts opts
@@ -13,17 +18,19 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-progressbar = ProgressBar.create(title: "Agent Jobs", total: AgentJob.count)
+if options[:queue]
+  progressbar = ProgressBar.create(title: "Agent Jobs", total: AgentJob.count)
 
-group = []
-AgentJob.find_each do |o|
-  group << [{ id: o.id }.stringify_keys]
-  next if o.id % 50_000 != 0
-  Sidekiq::Client.push_bulk({ 'class' => Bionomia::AgentParseWorker, 'args' => group })
-  progressbar.progress += group.size
   group = []
-end
-if group.size > 0
-  Sidekiq::Client.push_bulk({ 'class' => Bionomia::AgentParseWorker, 'args' => group })
-  progressbar.progress += group.size
+  AgentJob.find_each do |o|
+    group << [{ id: o.id }.stringify_keys]
+    next if o.id % 50_000 != 0
+    Sidekiq::Client.push_bulk({ 'class' => Bionomia::AgentParseWorker, 'args' => group })
+    progressbar.progress += group.size
+    group = []
+  end
+  if group.size > 0
+    Sidekiq::Client.push_bulk({ 'class' => Bionomia::AgentParseWorker, 'args' => group })
+    progressbar.progress += group.size
+  end
 end
--- bin/populate_agents.rb
+++ bin/populate_agents.rb
@@ -12,6 +12,10 @@ OptionParser.new do |opts|
     options[:truncate] = true
   end
 
+  opts.on("-q", "--queue", "Queue the jobs to parse agent strings.") do
+    options[:queue] = true
+  end
+
   opts.on("-h", "--help", "Prints this help") do
     puts opts
     exit
@@ -29,17 +33,19 @@ if options[:truncate]
   Sidekiq::Stats.new.reset
 end
 
-progressbar = ProgressBar.create(title: "Agent Jobs", total: AgentJob.count)
+if options[:queue]
+  progressbar = ProgressBar.create(title: "Agent Jobs", total: AgentJob.count)
 
-group = []
-AgentJob.find_each do |o|
-  group << [{ id: o.id }.stringify_keys]
-  next if o.id % 50_000 != 0
-  Sidekiq::Client.push_bulk({ 'class' => Bionomia::AgentCreateWorker, 'args' => group })
-  progressbar.progress += group.size
   group = []
-end
-if group.size > 0
-  Sidekiq::Client.push_bulk({ 'class' => Bionomia::AgentCreateWorker, 'args' => group })
-  progressbar.progress += group.size
+  AgentJob.find_each do |o|
+    group << [{ id: o.id }.stringify_keys]
+    next if o.id % 50_000 != 0
+    Sidekiq::Client.push_bulk({ 'class' => Bionomia::AgentCreateWorker, 'args' => group })
+    progressbar.progress += group.size
+    group = []
+  end
+  if group.size > 0
+    Sidekiq::Client.push_bulk({ 'class' => Bionomia::AgentCreateWorker, 'args' => group })
+    progressbar.progress += group.size
+  end
 end
