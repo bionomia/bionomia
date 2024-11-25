f3c694f97a7157b80f6fa2484f8419aeabcf260d
David Shorthouse <davidpshorthouse@gmail.com>
2024-11-25 12:05:02 -0500

    Fix GBIF citation checker

--- bin/gbif_citations.rb
+++ bin/gbif_citations.rb
@@ -12,16 +12,12 @@ OptionParser.new do |opts|
     options[:from] = from
   end
 
-  opts.on("-c", "--flush-caches", "Loop through processed articles and flush their caches") do
-    options[:caches] = true
-  end
-
   opts.on("-e", "--email", "Send out email notifications to users") do
     options[:email] = true
   end
 
-  opts.on("-d", "--delete", "Delete irrelevant article_occurrences entries because gbifID no longer exists") do
-    options[:delete] = true
+  opts.on("-c", "--flush-caches", "Loop through processed articles and flush their caches") do
+    options[:caches] = true
   end
 
   opts.on("-h", "--help", "Prints this help") do
@@ -30,25 +26,18 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-params = { max_size: 100_000_000 }
-
-if options[:from ]
-  params[:from] = options[:from]
-end
-
-tracker = Bionomia::GbifTracker.new(params)
-
-if options[:first]
+if options[:from]
+  params = {
+    max_size: 100_000_000,
+    from: options[:from]
+  }
+  tracker = Bionomia::GbifTracker.new(params)
   tracker.create_package_records
-end
-
-if options[:email]
+elsif options[:email]
   sm = Bionomia::SendMail.new
   sm.send_messages
   sm.mark_articles_sent
-end
-
-if options[:caches]
+elsif options[:caches]
   Article.where(processed: true).find_each do |a|
     a.flush_cache
     puts "#{a.id}".green
